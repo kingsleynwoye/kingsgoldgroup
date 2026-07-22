21f524ec43d029043455672469b3b7ddc3dbe052
Kingsley Nwoye <kingsleynwoye001@gmail.com>
2026-07-22 19:12:54 +0100

    Update codebase

---
+++ app/sitemap.ts
@@ -0,0 +1,43 @@
+import { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  const baseUrl = "https://www.kingsgoldgroup.com";
+
+  return [
+    {
+      url: baseUrl,
+      lastModified: new Date(),
+      priority: 1,
+    },
+    {
+      url: `${baseUrl}/about`,
+      lastModified: new Date(),
+      priority: 0.9,
+    },
+    {
+      url: `${baseUrl}/businesses`,
+      lastModified: new Date(),
+      priority: 0.9,
+    },
+    {
+      url: `${baseUrl}/contact`,
+      lastModified: new Date(),
+      priority: 0.8,
+    },
+    {
+      url: `${baseUrl}/faqs`,
+      lastModified: new Date(),
+      priority: 0.7,
+    },
+    {
+      url: `${baseUrl}/privacy-policy`,
+      lastModified: new Date(),
+      priority: 0.6,
+    },
+    {
+      url: `${baseUrl}/terms`,
+      lastModified: new Date(),
+      priority: 0.6,
+    },
+  ];
+}
